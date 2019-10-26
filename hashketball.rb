4cffe244a9f684fc1580b689e2991fd85a3b37b3
Conrad Robinson <robinsonc220@gmail.com>
2019-10-26 02:57:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -217,3 +217,18 @@ def big_shoe_rebounds
   end
   return rebounds
 end
+
+def most_points_scored
+  most_points = 0
+  mvp = ''
+  game_hash.each do |home_away, keys|
+    keys[:players].each do |player|
+      points = player[:points]
+      if points > most_points
+        most_points = points
+        mvp = player[:player_name]
+      end
+    end
+  end
+  mvp
+end
